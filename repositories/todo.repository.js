840addb2d10809e65e65956b8603cbe6018c59db
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-10-11 11:29:11 +0200

    Add todo repository

--- repositories/todo.repository.js
+++ repositories/todo.repository.js
@@ -1,5 +1,3 @@
-const { v4: uuid } = require("uuid");
-const { pool } = require("../utils/db");
 const { TodoRecord } = require("../records/todo.record");
 
 class TodoRepository {
@@ -11,54 +9,17 @@ class TodoRepository {
 
   static async insert(record) {
     TodoRepository._checkRecord(record);
-
-    record.id = record.id ?? uuid();
-
-    await pool.execute("INSERT INTO `todos` VALUES(:id, :title)", {
-      id: record.id,
-      title: record.title,
-    });
-
-    return record.id;
   }
 
   static async delete(record) {
     TodoRepository._checkRecord(record);
-
-    if (!record.id) {
-      throw new Error("Todo has no ID!");
-    }
-
-    await pool.execute("DELETE FROM `todos` WHERE `id` = :id", {
-      id: record.id,
-    });
   }
 
-  static async find(id) {
-    const [results] = pool.execute("SELECT * FROM `todos` WHERE `id` = :id", {
-      id: id,
-    });
-    return new TodoRecord(results[0]);
-  }
+  static async find(id) {}
 
-  static async findAll() {
-    const [results] = pool.execute("SELECT * FROM `todos`");
-    return results.map((result) => new TodoRecord(result));
-  }
+  static async findAll() {}
 
-  static async update(record) {
-    TodoRepository._checkRecord(record);
-
-    if (!record.id) {
-      throw new Error("Todo has no ID!");
-    }
-    record._validate();
-
-    await pool.execute("UPDATE `todos` SET `title` = :title WHERE `id` = :id", {
-      title: record.title,
-      id: record.id,
-    });
-  }
+  static async update(record) {}
 }
 
 module.exports = {
